fc2555f9cd42a486c9f4b6ee317a695daa320794
Sean Walsh <sdwalsh@mirango.io>
2017-06-29 18:08:00 -0400

    add database access structure

---
+++ models/db.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+/*
+Database / model access designed after Alex Edward's post on Go database organization
+http://www.alexedwards.net/blog/organising-database-access
+Code licensed under the MIT License
+
+Copyright 2015 Alex Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
+// Datastore interface contains all of our functions for the PostgreSQL database
+// this allows us to mock the database during tests!
+type Datastore interface {
+}
+
+// DB ...
+type DB struct {
+	*sql.DB
+}
+
+// NewDB initializes the database connection and returns a new DB struct pointer
+// databaseName is expected to be a PostgreSQL URL
+func NewDB(databaseName string) (*DB, error) {
+	db, err := sql.Open("postgres", databaseName)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return &DB{db}, nil
+}
