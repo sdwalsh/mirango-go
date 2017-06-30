a14c689af9d55ecaf5c1f4d52bbdc9bd99cf2ae9
Sean Walsh <sdwalsh@mirango.io>
2017-06-30 14:34:20 -0400

    conform to gofmt

--- models/posts.go
+++ models/posts.go
@@ -21,19 +21,21 @@ type Post struct {
 	CreatedAt   time.Time `db:"created_at"`
 }
 
+// Image struct based on image table in database
 type Image struct {
-	ID 			uuid.UUID 	`db:"id"`
-	UserID 		uuid.UUID 	`db:"user_id"`
-	Url 		string	 	`db:"url"`
-	Medium 		string 		`db:"medium"`
-	Small 		string	 	`db:"small"`
-	Caption 	string	 	`db:"caption"`
-	UpdatedAt 	time.Time 	`db:"updated_at"`
-	CreatedAt 	time.Time 	`db:"created_at"`
+	ID        uuid.UUID `db:"id"`
+	UserID    uuid.UUID `db:"user_id"`
+	URL       string    `db:"url"`
+	Medium    string    `db:"medium"`
+	Small     string    `db:"small"`
+	Caption   string    `db:"caption"`
+	UpdatedAt time.Time `db:"updated_at"`
+	CreatedAt time.Time `db:"created_at"`
 }
 
+// Tag struct based on tag table in database
 type Tag struct {
-	ID			uuid.UUID	`db:"id"`
-	Name		string 		`db:"name"`
-	Slug		string		`db:"slug"`
+	ID   uuid.UUID `db:"id"`
+	Name string    `db:"name"`
+	Slug string    `db:"slug"`
 }
