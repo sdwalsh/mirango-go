3397032e9d44ac4588da20479d4255254aec09c6
Sean Walsh <sdwalsh@mirango.io>
2017-07-02 11:13:53 -0400

    add user database functions

--- models/user.go
+++ models/user.go
@@ -17,3 +17,27 @@ type User struct {
 	LastOnlineAt time.Time `db:"updated_at"`
 	CreatedAt    time.Time `db:"created_at"`
 }
+
+// InsertUser ...
+func (db *DB) InsertUser(uname string, digest []byte, role string, email string, gpg string) (*User, error) {
+	var u User
+	sql := "INSERT INTO users (uname, digest, role, email, gpg) VALUES ($1, $2, $3, $4, $5) RETURNING *"
+	err := db.Get(&u, sql, uname, digest, role, email, gpg)
+	return &u, err
+}
+
+// GetUserByEmail ...
+func (db *DB) GetUserByEmail(email string) (*User, error) {
+	var u User
+	sql := "SELECT * FROM users WHERE email = $1"
+	err := db.Get(&u, sql, email)
+	return &u, err
+}
+
+// GetUserByID ...
+func (db *DB) GetUserByID(user uuid.UUID) (*User, error) {
+	var u User
+	sql := "SELECT * FROM users WHERE id = $1"
+	err := db.Get(&u, sql, user)
+	return &u, err
+}
